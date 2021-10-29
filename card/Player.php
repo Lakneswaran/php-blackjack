e72c8e3a37c92d67a353272bacd65f510c0004e5
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2021-10-29 12:30:45 +0200

    player and index
    Player  working - get two cards .

--- card/Player.php
+++ card/Player.php
@@ -4,28 +4,46 @@ class Player{
 
     private $cards = [];
     private $lost = false;
+    
+
+    public function __construct($deck)
+    {
+        for($i=0;$i<2;$i++){
+            $this->cards[]= $deck->drawCard();
+        }
+    }
 
     
-    public function hit(){
-        
+    public function hit($deck){
+        $this->cards[]= $deck->drawCard();
+        $score = $this->getScore();
+        if($score<=21){
+            $this->lost =true;
+
+        }
     }
 
 
     public function surrender(){
-        
+         $this->lost = true;
     }
 
 
     public function getScore(){
         $score=0;
-        foreach($cards as $card){
+        foreach($this->cards as $card){
             $score += $card->getvalue();
         }
+        return $this->score;
     }
 
 
     public function hasLost(){
-        
+        return $this->lost;
+    }
+
+    public function playcards(){
+        return $this->cards;
     }
 
 }
--- index.php
+++ index.php
@@ -5,15 +5,46 @@ declare(strict_types=1);
 require 'card/Suit.php';
 require 'card/Card.php';
 require 'card/Deck.php';
+require 'card/Blackjack.php';
+require 'card/Dealer.php';
+require 'card/Player.php';
 
-$deck = new Deck();
-$deck->shuffle();
-foreach($deck->getCards() AS $card) {
-    echo $card->getUnicodeCharacter(true);
-    echo '<br>';
-
-
+session_start();
+if (!isset($_SESSION['Blackjack'])) {
+    $blackjack = new Blackjack();
+    $_SESSION["Blackjack"] = $blackjack;
+}else{
+    $blackjack = $_SESSION['Blackjack'];
 }
+
+var_dump($blackjack->getPlayer()->playcards());
+
+// $deck = new Deck();
+// $deck->shuffle();
+// foreach($deck->getCards() AS $card) {
+//     echo $card->getUnicodeCharacter(true);
+//     echo '<br>';
+
+
+// }
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+    <div id="communicate">
+        <button type="submit" name="hit" >HIT</button>
+        <button type="submit" name="stand">STAND</button>
+        <button type="submit" name="surrender">SURRENDER</button>
+
+    </form>
+</body>
+</html>
